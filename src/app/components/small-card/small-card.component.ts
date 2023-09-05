99ef75fc93ab0769757ec4e8ddb59a59998bf938
Lincoln <lincolntaranto@gmail.com>
2023-09-05 14:11:11 -0300

    Added initial responsiveness and dynamic components.

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css', './small-card.responsive.component.css']
 })
 export class SmallCardComponent {
+  @Input()
+  pictureCover:String = ""
+  @Input()
+  cardTitle:String = ""
 
 }
